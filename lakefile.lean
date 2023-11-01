c5d3cfbe795802751756994bd2078896d687873c
adamtopaz <github@adamtopaz.com>
2023-11-01 06:34:14 -0600

    cleanup

--- lakefile.lean
+++ lakefile.lean
@@ -5,11 +5,7 @@ package «lean_find_with_gpt» {
   -- add package configuration options here
 }
 
+@[default_target]
 lean_lib «LeanFindWithGpt» {
   -- add library configuration options here
-}
-
-@[default_target]
-lean_exe «lean_find_with_gpt» {
-  root := `Main
 }
